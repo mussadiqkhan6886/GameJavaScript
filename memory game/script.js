a54528ef1e8215a05bc5ae43bf3dd9711282b7d1
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-03-16 00:23:17 +0500

    started memory game

---
+++ memory game/script.js
@@ -0,0 +1,50 @@
+let cardGame = [
+    {
+        name: 'fries',
+        img: 'img/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'img/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'img/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'img/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'img/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'img/pizza.png'
+    },
+    {
+        name: 'fries',
+        img: 'img/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'img/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'img/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'img/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'img/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'img/pizza.png'
+    }
+]
